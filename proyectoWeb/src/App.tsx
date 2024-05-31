41ec6c1e9fc28d8301d272cf6a9d8e456e895cc4
Mark <20460475@colima.tecnm.mx>
2024-05-31 17:57:42 -0600

    Tests

--- proyectoWeb/src/App.tsx
+++ proyectoWeb/src/App.tsx
@@ -4,6 +4,8 @@ import './App.css'
 function App() {
   const navigate = useNavigate();
 
+
+
   return (
     <>
       <div>
---
+++ proyectoWeb/src/Components/App.test.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { it, expect } from 'vitest'
+import Login from './Login';
+import { BrowserRouter as Router } from 'react-router-dom'; // Importa BrowserRouter para envolver el componente Login
+
+
+it('renders without crashing', () => {
+  render(
+    <Router> {/* Envuelve el componente Login con BrowserRouter */}
+      <Login />
+    </Router>
+  );
+  const iniciarSesionElements = screen.getAllByText('Iniciar Sesión');
+  expect(iniciarSesionElements.length).toBeGreaterThan(0); // Verifica que al menos un elemento esté presente
+  expect(screen.getByPlaceholderText('Correo')).toBeInTheDocument();
+  expect(screen.getByPlaceholderText('Contraseña')).toBeInTheDocument();
+  expect(screen.getByText('Registrar')).toBeInTheDocument();
+});
+
+
+it('updates state when input values change', () => {
+  render(
+    <Router> {/* Envuelve el componente Login con BrowserRouter */}
+      <Login />
+    </Router>
+  );
+  fireEvent.change(screen.getByPlaceholderText('Correo'), { target: { value: 'test@test.com' } });
+  fireEvent.change(screen.getByPlaceholderText('Contraseña'), { target: { value: 'password123' } });
+  expect(screen.getByPlaceholderText('Correo')).toHaveValue('test@test.com');
+  expect(screen.getByPlaceholderText('Contraseña')).toHaveValue('password123');
+});
+
+
+
